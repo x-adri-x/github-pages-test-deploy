0b20dec33a34ff793dfbe9dfd04723789aecfa47
Adrienn <adriennlenart@yahoo.com>
2024-02-12 21:24:07 +0100

    ci: use GITHUB_REPOSITORY env variable

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/github-pages-test-deploy/',
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
