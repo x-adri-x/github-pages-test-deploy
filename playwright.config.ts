e4c19c4d5d6ab9c7e67917529647585bbf6caf0f
Adrienn <adriennlenart@yahoo.com>
2024-02-13 12:39:20 +0100

    test: add test configuration for Github pages

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,12 @@
 import process from 'node:process'
 import { defineConfig, devices } from '@playwright/test'
 
+const formBaseURL = (baseURL: string) => {
+  const repo = process.env.GITHUB_REPOSITORY
+  const basePath = repo ? `/${repo.split('/')[1]}/` : ''
+
+  return `${baseURL}${basePath}`
+}
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -34,7 +40,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: formBaseURL('http://localhost:5173'),
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
